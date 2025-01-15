0b30162bf3a9daecd39fbccedb20501884351110
ninenotes <111536344+ninenotes@users.noreply.github.com>
2025-01-15 14:30:11 +0700

    refs-func.js
    refs-func.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Calculator2} from './calculator'
-import { EventData2 } from './event-data';
+import  RefsFunc from './refs-func'
 
 
 function App() {
   return (
-  <EventData2/>
+  <RefsFunc/>
   );
 }
 
