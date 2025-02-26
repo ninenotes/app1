4a01059692f1c84ba85d96af8951eba9d3100673
ninenotes <111536344+ninenotes@users.noreply.github.com>
2025-02-26 15:50:52 +0700

    Input type file
    Input type file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import FormText from './form-text';
-
+import React from 'react'
+import FormSelectFile from './form-select-file';
 export default function App() {
-return <FormText/>
+return <FormSelectFile/>
 }
