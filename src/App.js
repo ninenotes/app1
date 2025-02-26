82506e6a8aa78a6ffccd9e73f550681209264c43
ninenotes <111536344+ninenotes@users.noreply.github.com>
2025-02-26 15:00:43 +0700

    Input type text password
    Input type text password

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import React from 'react'
-import Router2 from './router2';
-import Router3 from './router3';
+import React from 'react';
+import FormText from './form-text';
 
-function App() {
-  return (
-   <Router3/>
-  );
+export default function App() {
+return <FormText/>
 }
-
-
-export default App;
-
